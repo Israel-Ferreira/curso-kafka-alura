0e73d6f6ae25d696ff0bcb1f98d4f93d1240ebf4
israel.souza <israel.souza@summit-bra.com>
2022-10-27 17:37:05 -0300

    producer java

--- ecommerce/src/main/java/io/codekaffee/curso/kafka/Main.java
+++ ecommerce/src/main/java/io/codekaffee/curso/kafka/Main.java
@@ -2,6 +2,7 @@ package io.codekaffee.curso.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -12,11 +13,18 @@ public class Main {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "ecommerce-producer");
 
 
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+        try(KafkaProducer<String,String> producer =  new KafkaProducer<>(props)) {
+            String value =  "1223344,9999,99931";
 
+            System.out.println("TESTE");
 
-        System.out.println("Hello world!");
+            ProducerRecord<String,String> producerRecord =  new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value,value);
+            producer.send(producerRecord);
+
+        }
+
     }
 }
