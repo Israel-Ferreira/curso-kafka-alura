b0cbce20e95f827b69c8a21368097fb128dfb130
israel.souza <israel.souza@summit-bra.com>
2022-10-27 16:28:58 -0300

    Criando o producer

---
+++ ecommerce/src/main/java/io/codekaffee/curso/kafka/Main.java
@@ -0,0 +1,22 @@
+package io.codekaffee.curso.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ ecommerce/src/main/java/io/codekaffee/curso/kafka/models/NewOrder.java
@@ -0,0 +1,5 @@
+package io.codekaffee.curso.kafka.models;
+
+
+public class NewOrder {
+}
