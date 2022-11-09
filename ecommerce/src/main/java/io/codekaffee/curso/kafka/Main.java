b60f5f0abcd8bfa0e97340c72910de83b5e0e5a1
Israel-Ferreira <israelfsouza@hotmail.com>
2022-11-09 15:28:35 -0300

    Otimizando imports

--- ecommerce/src/main/java/io/codekaffee/curso/kafka/Main.java
+++ ecommerce/src/main/java/io/codekaffee/curso/kafka/Main.java
@@ -1,12 +1,5 @@
 package io.codekaffee.curso.kafka;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
 import java.util.UUID;
 
 public class Main {
